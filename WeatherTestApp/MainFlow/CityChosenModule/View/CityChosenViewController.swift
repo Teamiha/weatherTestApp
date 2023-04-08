977c7a37c34da1a73d006536ba5d0f2bc83ff5c7
Mikhail Svetlov <jegnum@gmail.com>
2023-04-08 04:30:26 +0400

    Add bluprint TableViewCityChosen

--- WeatherTestApp/MainFlow/CityChosenModule/View/CityChosenViewController.swift
+++ WeatherTestApp/MainFlow/CityChosenModule/View/CityChosenViewController.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
-class CityChosenViewController: UIViewController {
+class CityChosenViewController: UITableViewController {
+    
+    //MARK: - Properties
     
     var presenter: CityChosenPresenterProtocol!
+    private let cellID = "ID"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
 
         // Do any additional setup after loading the view.
     }
@@ -27,6 +31,24 @@ class CityChosenViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+       
+        content.text = "Test"
+        cell.contentConfiguration = content
+        cell.selectionStyle = .none
+        
+        return cell
+    }
 
 }
 
@@ -38,7 +60,8 @@ extension CityChosenViewController: CityChosenViewProtocol {
     func failure(error: Error) {
         return
     }
-    
-    
-    
 }
+
+
+
+
